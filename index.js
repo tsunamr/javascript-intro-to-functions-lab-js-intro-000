416c3201ae4b408d5c4878ab8c9d1124b007bdda
sunam RImal Nepali <tsunamr@gmail.com>
2020-11-25 18:20:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
 if (string.toLowerCase()){
 return "I can't hear you!" }
-}
+
 var sayhi = "yes indeed!";
  else if (string.toUpperCase()){
   return sayhi.toUpperCase() }
